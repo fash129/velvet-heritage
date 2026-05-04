96ee68f72270a2e3e1803debff4ed1890387f4fe
fash129 <fashionistafcih@gmail.com>
2026-05-04 20:35:09 +0300

    Add files via upload

--- theme.js
+++ theme.js
@@ -1,19 +1,28 @@
-const button =document.getElementById("themeToggle");
+{const button = document.getElementById("theme-switch");
+ const savedTheme = localStorage.getItem("theme");
+ const icon = button.querySelector("i");
+ if (savedTheme === "light") {
+ document.body.classList.add("light");
+ if(icon)
+ icon.classList.replace('fa-moon','fa-sun');
+ } else {
+   document.body.classList.remove("light");
+   if(icon) 
+   icon.classList.replace('fa-moon','fa-sun');
+ }
 
-const savedTheme = localStorage.getItem("theme");
+ button.onclick = function(){
+     document.body.classList.toggle("light");  
+ if (document.body.classList.contains("light") ) {
+     localStorage.setItem("theme", "light");
+     if(icon)
+ icon.classList.replace('fa-moon','fa-sun');
 
-if (savedTheme === "light") {
-    document.body.classList.add("light");
-} else {
-    document.body.classList.remove("light");
-}
+   } else {
+     localStorage.setItem("theme", "dark");
+     if(icon) 
+   icon.classList.replace('fa-moon','fa-sun');
+   }
+ }};
 
-button.onclick = function () {
-    document.body.classList.toggle ("light");
 
-    if (document.body.classList.contains("light")) {
-        localStorage.setItem("theme","light");
-    } else {
-        localStorage.setItem("theme","dark");
-    }
-}
